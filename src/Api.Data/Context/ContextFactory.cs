8056ca18f7baff5674a128fad3150ed10d7da391
aecio <aecio@supermercadosbh.com.br>
2021-04-07 10:31:28 -0300

    #04 - Adicionando MyContext

---
+++ src/Api.Data/Context/ContextFactory.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace src.Api.Data.Context
+{
+  /// <summary>
+  /// Essa classe serve para que possamos criar bancos de dados tudo em tempo de designer, 
+  /// vai prover a conexão com o banco de dados para que possamos criar nossas tabelas
+  /// </summary>
+  public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+  {
+    public MyContext CreateDbContext(string[] args)
+    {
+      // Usado para criar as Migrações
+      var connectionString = "Host=localhost;Database=Estudo;Username=postgres;Password=x;Timeout=300";
+      var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+      optionsBuilder.UseNpgsql(connectionString);
+
+      return new MyContext(optionsBuilder.Options);
+    }
+  }
+}
