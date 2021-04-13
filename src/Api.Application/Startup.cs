5f69e03c2cee4c38987f8b035d0e22068a4ce7f2
aecio <aecio@supermercadosbh.com.br>
2021-04-13 16:58:02 -0300

    #14 - Implementando Swagger

--- src/Api.Application/Startup.cs
+++ src/Api.Application/Startup.cs
@@ -1,6 +1,7 @@
 using Api.CrossCutting.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,7 +27,13 @@ namespace Application
       services.AddControllers();
       services.AddSwaggerGen(c =>
       {
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Application", Version = "v1" });
+        c.SwaggerDoc("v1",
+            new OpenApiInfo
+            {
+              Title = "Curso de AspNetCore",
+              Version = "v1",
+              Description = "Exemplo de Api REST criada com o ASP.NET Core"
+            });
       });
     }
 
@@ -36,8 +43,24 @@ namespace Application
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
+
+        #region Configurações do Swagger
+
+        //Ativando Middlewares para use do Swagger
         app.UseSwagger();
-        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Application v1"));
+        app.UseSwaggerUI(c =>
+        {
+          c.RoutePrefix = string.Empty;
+          c.SwaggerEndpoint("/swagger/v1/swagger.json", "Curso de API com AspNetCore");
+        });
+
+        // Redireciona o Link para o Swagger, quando acessar a rota principal
+        var option = new RewriteOptions();
+        option.AddRedirect("^$", "swagger");
+        app.UseRewriter(option);
+
+        #endregion
+
       }
 
       app.UseRouting();
